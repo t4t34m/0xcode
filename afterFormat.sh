af6338aa0a82daa0cd18ae68742e09f88a6caffa
T4T34M <80798479+t4t34m@users.noreply.github.com>
2022-06-24 18:45:16 -0500

    updated(ADDED)

--- afterFormat.sh
+++ afterFormat.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/bash
 # How to run Best with no root just ./afterFormat.sh
-
+# 2022-06-24
 clear
 coption(){
-    printf '\e[1;33m\n# Enter Number ( 1 ) to start.. OR Hit \e[1;38;5;112mEnter to Skip\n\e[0m'
-    read -p $'\e[1;37m# Enter Now : \e[0m' answer
+    read -p $'\n\e[1;37m#: \e[0m' answer
     }
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start Update & Upgrade \e[0m\n"""  
+printf """\n\e[0m#1\e[1;37m INSTALL -  updating & upgrading  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m\n"""  
 coption
 if [[ "$answer" == "1" ]]; 
 then
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Do you want to add-repos ubuntu ?  only 1 time after format..(Skip)\e[0m\n""" 
+printf """add-repos ubuntu? 1) yes , 2) Skip ... (ONLY 1 TIME AFTER FORMAT)\n""" 
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -39,14 +38,26 @@ sudo apt clean -y
 sudo apt autoclean -y
 fi
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start INSTALL Python (libs/dev/dos/pip/..etc) \e[0m\n"""  
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;37m INSTALL - Python (libs/dev/dos/pip/..etc)  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m\n"""
 coption
 if [[ "$answer" == "1" ]]; 
 then
 sudo apt-get install synaptic tor libpython2-stdlib python2-minimal python-olefile python-is-python2 python-pkg-resources python2-dev python2 libssl1.0.0 python-pip-whl perl-doc python-mysqldb libpcap0.8-dev figlet git g++ python3 python-dev-is-python2 python3-dev libssl1.1 libssl-dev libwww-mechanize-shell-perl libio-socket-socks-perl libmoose-perl cpanminus python3-pip python3-mysqldb python3-gevent python3-gevent-websocket openssl build-essential libreadline-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev libyaml-dev gawk bison libffi7 libffi-dev libgdbm-dev libncurses5-dev libtool sqlite3 libgmp-dev gnupg2 dirmngr gtk3-nocsd bleachbit stacer basez gnome-terminal 2to3 websploit dirsearch dmitry gobuster checkinstall ruby ruby-dev  libcurl4-openssl-dev unrar bacula-sd xclip onesixtyone libgtk3-nocsd0 libxml2 pkg-config python-opengl python-pil python-pyrex python3-pyside2.qtopengl lynx libpython2-dev libpython-all-dev python-all python-all-dev python-tk python-numpy libgle3 xsltproc libgmp-dev inotify-tools torsocks proxychains4 tor
+sudo apt install software-properties-common -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin ca-certificates containerd.io docker-compose-plugin -y
+sudo apt install npm nodejs gcc make yarn gmtp nodejs-legacy gcc-mingw-w64-i686 mingw-w64 -y
+sudo apt install glogg zstd enscript gnuplot grads hp2xx html2ps libwmf-bin mplayer povray transfig ufraw-batch inkscape libjxr-tools -y
+sudo apt install exiv2 imagemagick libimage-exiftool-perl jq imagemagick-doc elinks goldeneye perl-tk spikeproxy network-manager-openvpn-gnome -y
+sudo apt install libjs-dojo-core libxml2-dev libboost-dev qt5-default qtbase5-dev-tools qtdeclarative5-dev libqt5svg5-dev libqt5websockets5-dev cmake libportmidi0 libasound2 -y
+sudo apt install libusbmuxd-dev ifuse xvfb ideviceinstaller python-imobiledevice libimobiledevice-utils libimobiledevice6 -y
+sudo apt install libev-dev libnl-3-dev libnl-genl-3-dev libnl-route-3-dev -y
+sudo apt install ruby-full finger xsel v4l2loopback-dkms v4l-utils ffmpeg -y
 fi 
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start INSTALL phpstorm testssl docker go \e[0m\n"""  
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;37m INSTALL - phpstorm testssl docker go snaps  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -54,9 +65,17 @@ sudo snap install phpstorm --classic
 sudo snap install docker
 sudo snap install testssl
 sudo snap install go --classic
+sudo snap install pictool
+sudo snap install core
+sudo snap install multipass
+sudo snap install pynsource
+sudo snap install cmake --classic
 fi
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start INSTALL PERL: cpan Eksblowfish SSH Net \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;37m INSTALL - Perl / Cpan / EksblowFish / SSH::NET  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -67,7 +86,12 @@ sudo cpan -i Crypt::Eksblowfish::Bcrypt
 sudo cpan -i Crypt::EksBlowfish
 fi 
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start INSTALL pip3/mods requests,multiprocessing,..etc reqs \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;37m INSTALL - pip3/mods requests,multiprocessing,..etc reqs  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
+
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -83,15 +107,28 @@ pip3 install pandas --trusted-host pypi.org --trusted-host files.pythonhosted.or
 pip3 install mysqlclient --user
 pip install Scrapy 
 pip3 install selenium --user
-pip3 install fierce
-pip3 install python-dateutil==2.8.1 --user
-pip3 install typing --user
-pip3 install ujson
-pip install multiprocess
-sudo pip3 install bandit
+sudo pip3 install -U selenium
+pip install --no-binary :all: faradaysec --user
+sudo python3 -m pip install --upgrade requests
+#opendrop send -r 0 -f https://owlink.org --url
+pip install aiodns dnspython==1.16.0 opendrop html2text isort mysql.connector mysql colorlog flask-cors faradaysec ip2loc ifaddr pwn poster==0.4 autopep8 dnspython3 fierce python-dateutil==2.8.1 typing ujson bandit multiprocess --user
+sudo pip install aiodns dnspython==1.16.0 opendrop html2text isort mysql.connector mysql colorlog flask-cors faradaysec ip2loc ifaddr pwn poster==0.4 autopep8 dnspython3 fierce python-dateutil==2.8.1 typing ujson bandit multiprocess --user
+pip3 install aiodns dnspython==1.16.0 opendrop html2text isort mysql.connector mysql colorlog flask-cors faradaysec ip2loc ifaddr pwn poster==0.4 autopep8 dnspython3 fierce python-dateutil==2.8.1 typing ujson bandit multiprocess --user
+sudo pip3 install aiodns dnspython==1.16.0 opendrop html2text isort mysql.connector mysql colorlog flask-cors faradaysec ip2loc ifaddr pwn poster==0.4 autopep8 dnspython3 fierce python-dateutil==2.8.1 typing ujson bandit multiprocess --user
+pip install imageio webdriver_manager chromedriver_autoinstaller chromedriver_binary undetected-chromedriver selenium-stealth tiktokpy pystyle playwright-stealth TikTokApi --user
+sudo pip install imageio webdriver_manager chromedriver_autoinstaller chromedriver_binary undetected-chromedriver selenium-stealth tiktokpy pystyle playwright-stealth TikTokApi --user
+pip3 install imageio webdriver_manager chromedriver_autoinstaller chromedriver_binary undetected-chromedriver selenium-stealth tiktokpy pystyle playwright-stealth TikTokApi --user
+sudo pip3 install imageio webdriver_manager chromedriver_autoinstaller chromedriver_binary undetected-chromedriver selenium-stealth tiktokpy pystyle playwright-stealth TikTokApi --user
+
 fi
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start INSTALL gem gobuster wpscan nokogiri \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;37m INSTALL - gem gobuster wpscan nokogiri  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
+
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -104,7 +141,14 @@ source ~/.profile
 source ~/.bashrc
 fi 
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Start APACHE/PHPMYADMIN/MYSQL/PHP \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;37m INSTALL - APACHE/PHPMYADMIN/MYSQL/PHP \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
+ 
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -121,7 +165,14 @@ sudo systemctl enable mysql
 sudo chown -R $USER:$USER /var/www/html
 fi 
 clear
-printf """\n[\e[1;38;5;112m + \e[0m] \e[1;37m Create /opt/backbox \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;31m Done - APACHE/PHPMYADMIN/MYSQL/PHP  \e[0m"""
+printf """\n#8\e[1;37m INSTALL - Create /opt/backbox \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -130,26 +181,15 @@ then
     printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m Found /opt/backbox \e[0m\n"""
 else
     cd /opt
-    printf "[1] password to create /opt/backbox"
+    printf "enter password to create /opt/backbox"
     sudo mkdir backbox
-    printf "[1] password to chown $USER "
+    printf "enter password to chown $USER"
     sudo chown -R $USER:$USER /opt/backbox
+    chmod +x -R /opt/backbox
 fi
-if [ -d "/opt/backbox/wpscan" ] 
+if [ -d "/opt/backbox/0xcode" ] 
 then
-    printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m Found WPscan \e[0m\n"""
-else
-    sudo chown -R $USER:$USER /opt/backbox
-    cd /opt/backbox
-    git clone https://github.com/wpscanteam/wpscan.git
-    sleep 3
-    cd wpscan/
-    sudo gem install bundler && bundle config set --local without 'test'
-    sudo chown -R $USER:$USER /opt/backbox
-fi
-if [ -d "/opt/backbox/jm511hacker" ] 
-then
-    printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m Found jm511hacker \e[0m\n"""
+    printf """\nFound 0xcode \n"""
 else
     sudo chown -R $USER:$USER /opt/backbox
     cd /opt/backbox/
@@ -158,9 +198,22 @@ else
     curl -sLO http://archive.ubuntu.com/ubuntu/pool/universe/s/sslscan/sslscan_2.0.7-1_amd64.deb && sudo apt update && sudo dpkg -i sslscan_2.0.7-1_amd64.deb && sudo apt install -f -y
     wget https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/code_1.63.2-1639562499_amd64.deb && sudo apt update -y && sudo dpkg -i code_1.63.2-1639562499_amd64.deb
 fi
+rm /opt/backbox/sslscan_2.0.7-1_amd64.deb
+rm /opt/backbox/code_1.63.2-1639562499_amd64.deb
 clear
+
 fi 
-printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m Start INSTALL Tools : sqlitebrowser,whatweb,dmitry...etc \e[0m\n"""
+clear
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;31m Done - APACHE/PHPMYADMIN/MYSQL/PHP  \e[0m"""
+printf """\n#8\e[1;31m Done - Create /opt/backbox  \e[0m"""
+printf """\n#9\e[1;37m INSTALL - Tools : sqlitebrowser,whatweb,dmitry...etc \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
+
 coption
 if [[ "$answer" == "1" ]];
 then
@@ -171,7 +224,17 @@ sudo apt install jboss-autopwn whatweb gnome-terminal alacarte pnscan -y
 sudo apt install libqt4-network libqtcore4 libqtocore4 libqtgui4 -y
 fi
 clear
-printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m Java 17 install \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;31m Done - APACHE/PHPMYADMIN/MYSQL/PHP  \e[0m"""
+printf """\n#8\e[1;31m Done - Create /opt/backbox  \e[0m"""
+printf """\n#9\e[1;31m Done - Tools : sqlitebrowser,whatweb,dmitry...etc  \e[0m"""
+printf """\n#10\e[1;37m INSTALL - Java 17 install  \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
+
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -181,7 +244,17 @@ sudo apt -y install oracle-java17-set-default -y
 source /etc/profile.d/jdk.sh
 fi
 clear
-printf """\n[\e[1;38;5;120m + \e[0m] \e[1;37m proxychains4 update && upgrade  \e[0m\n"""
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;31m Done - APACHE/PHPMYADMIN/MYSQL/PHP  \e[0m"""
+printf """\n#8\e[1;31m Done - Create /opt/backbox  \e[0m"""
+printf """\n#9\e[1;31m Done - Tools : sqlitebrowser,whatweb,dmitry...etc  \e[0m"""
+printf """\n#10\e[1;31m Done - Java 17 install  \e[0m"""
+printf """\n#11\e[1;37m INSTALL -  proxychains4 update && upgrade \e[0m\n\n1) \e[1;38;5;112m to Install\e[0m\n2) \e[1;38;5;112m to Skip\e[0m"""
 coption
 if [[ "$answer" == "1" ]]; 
 then
@@ -193,56 +266,25 @@ proxychains4 sudo apt-get -o Dpkg::Options::="--force-overwrite" upgrade -y
 proxychains4 sudo dpkg --configure -a
 fi
 clear
-echo "# DISABLE UFW sudo ufw disable ..."
-sudo ufw disable
-echo "# Cleaning ..."
-sudo chown -R $USER:$USER /opt/backbox
-sudo journalctl --vacuum-time=200M
-sudo du -sh ~/.cache/thumbnails
-sudo rm -rf ~/.cache/thumbnails/*
-sudo du -sh ~/.cache/thumbnails
-rm /opt/backbox/sslscan_2.0.7-1_amd64.deb
-rm /opt/backbox/code_1.63.2-1639562499_amd64.deb
-clear
-echo "# done ...."
+printf """\n#1\e[1;31m Done - updating & upgrading  \e[0m"""  
+printf """\n#2\e[1;31m Done - Python (libs/dev/dos/pip/..etc)  \e[0m"""
+printf """\n#3\e[1;31m Done - phpstorm testssl docker go snaps  \e[0m"""
+printf """\n#4\e[1;31m Done - Perl / Cpan / EksblowFish / SSH::NET  \e[0m"""
+printf """\n#5\e[1;31m Done - pip3/mods requests,multiprocessing,..etc reqs  \e[0m"""
+printf """\n#6\e[1;31m Done - gem gobuster wpscan nokogiri  \e[0m"""
+printf """\n#7\e[1;31m Done - APACHE/PHPMYADMIN/MYSQL/PHP  \e[0m"""
+printf """\n#8\e[1;31m Done - Create /opt/backbox  \e[0m"""
+printf """\n#9\e[1;31m Done - Tools : sqlitebrowser,whatweb,dmitry...etc  \e[0m"""
+printf """\n#10\e[1;31m Done - Java 17 install  \e[0m"""
+printf """\n#11\e[1;31m Done - proxychains4 update && upgrade  \e[0m"""
+printf """\n\e[1;38;5;112m DONE INSTALL  \e[0m"""
+
 printf """
 [ CMD ]
-#
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0
+#---------------------
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libgtk3-nocsd.so.0 && source ~/.bashrc 
 sudo grep -r LD_PRELOAD /etc $HOME
-source ~/.bashrc 
-sudo ./config --openssldir=/usr/local/ssl
-
-#------------CMD-----------#
-testssl --fast target.com
-testssl --beast target.com
-testssl --show-certificate target.com | grep \" show\"
-testssl -p -s -f -U -S -P --jsonfile-pretty=mxtest.json https://target.com
-sslscan --no-failed target.com
-sslscan --http --bugs --verbose --show-certificate target.com
----
-nmap target.com --open 443 --open --defeat-rst-ratelimit
-hydra smtp.gmail.com smtp -l xx@gmail.com -P pwd.lst -s 465 -S -v -V
-curl -v --silent http://target.com/ 2>&1 | grep -hrio \"\b[a-z0-9.-]\+@[a-z0-9.-]\+\.[a-z]\{2,4\}\+\b\"
-getent hosts target.com | awk '{ print $0 }'
-wget --execute robots=off --recursive --no-parent --continue target.com
----
-sudo docker run -it --rm wpscanteam/wpscan --url https://target.com/ --no-banner --random-user-agent
-htpasswd -bnBC 10 '' password | tr -d ':\\\\n' | sed 's/$2y/$2a/'
-openssl base64 -e <<< '511'
-openssl base64 -d <<< 'NTExCg=='
----
-echo -e '\U0001f602'
-echo \"xyzvb12345-Ab-C5678-dEf\" | sed '0,/-/s//\//'
-echo \"Start Update/Upgrade\" | figlet
-echo '{\"TEST\":\"TEST\"}\"}' | python3 -m simplejson.tool
-echo -n \"TEST\" | md5sum - 
-echo -n \"TEST\" | sha256sum -
----
-find . -iname \"*.sql\" | while read -r x; do cat $x | grep \"@gmail.com\"; done
-find /opt/backbox/ -iname '*gmail*' -print
-find /opt/backbox/ -iname \"*.sql\" | while read -r x; do cat $x | grep -E -o \"gmail\"; done
----
-tor --hash-password \"PASSWORD_HERE\"
+(echo 'deb [arch=amd64, trusted=yes] https://nuts.vysor.io/apt ./' | sudo tee /etc/apt/sources.list.d/vysor.list) && sudo apt update && sudo apt install vysor 
 sudo tor --service install -options ControlPort 9051
+#---------------------
 """
